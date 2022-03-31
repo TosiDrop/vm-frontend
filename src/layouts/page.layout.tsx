6d7e254a77dd3aa8a697b9f01295d2dc63bc638b
Chris Gianelloni <cgianelloni@cloudstruct.net>
2022-03-31 18:06:29 -0400

    Update React Router logic
    Change to using Link in our menu, so React Router works correctly for
    them.
    
    Signed-off-by: Chris Gianelloni <cgianelloni@cloudstruct.net>

--- src/layouts/page.layout.tsx
+++ src/layouts/page.layout.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Dashboard from '../pages/dashboard';
 import Rewards from '../pages/rewards.page';
 import RewardsHistory from '../pages/rewards-history';
 import Feedback from '../pages/feedback';
 
 function Page() {
-    return <Router>
-        <Routes>
-            <Route path="/" element={<Rewards />} />
-            <Route path="/history" element={<RewardsHistory />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/feedback" element={<Feedback />} />
-        </Routes>
-    </Router>
+    return <Routes>
+        <Route path="/" element={<Rewards />} />
+        <Route path="/history" element={<RewardsHistory />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/feedback" element={<Feedback />} />
+    </Routes>
 }
 
 export default Page;
