7e57934df088803a32dd831ff31801755f8f7308
aryelciu001 <ryelcius.alfredo29@gmail.com>
2022-04-25 19:30:39 +0800

    chore/client: run prettier

--- client/src/layouts/header.layout.tsx
+++ client/src/layouts/header.layout.tsx
@@ -1,9 +1,9 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faSun } from '@fortawesome/free-solid-svg-icons';
-import WalletSelectorComponent from '../components/wallet-selector/wallet-selector.component';
-import WalletApi, { WalletKeys } from '../services/connectors/wallet.connector';
-import logo from '../assets/tosidrop_logo.png';
-import './header.layout.scss';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faSun } from "@fortawesome/free-solid-svg-icons";
+import WalletSelectorComponent from "../components/wallet-selector/wallet-selector.component";
+import WalletApi, { WalletKeys } from "../services/connectors/wallet.connector";
+import logo from "../assets/tosidrop_logo.png";
+import "./header.layout.scss";
 
 interface Params {
     toggleMenu: () => void;
@@ -12,28 +12,42 @@ interface Params {
     connectedWallet: WalletApi | undefined;
     wrongNetwork: boolean | undefined;
 }
-function Header({ toggleMenu, toggleTheme, connectWallet, connectedWallet, wrongNetwork }: Params) {
-
-    return <div className='header'>
-        <div className='header-title'>
-            <div className='logo-container'>
-                <img src={logo} className="logo" alt=''></img>
-            </div>
-            <p className='title-text'>TosiDrop</p>
-            <div className='header-filler'></div>
-            <div className='header-wallet-selector noselect'>
-                <WalletSelectorComponent connectedWallet={connectedWallet} connectWallet={connectWallet} wrongNetwork={wrongNetwork} />
-            </div>
-            <div className='last'>
-                <button className="button light-button" onClick={toggleTheme}>
-                    <FontAwesomeIcon icon={faSun} />
-                </button>
-                <button className="button menu-button" onClick={toggleMenu}>
-                    <FontAwesomeIcon icon={faBars} />
-                </button>
+function Header({
+    toggleMenu,
+    toggleTheme,
+    connectWallet,
+    connectedWallet,
+    wrongNetwork,
+}: Params) {
+    return (
+        <div className="header">
+            <div className="header-title">
+                <div className="logo-container">
+                    <img src={logo} className="logo" alt=""></img>
+                </div>
+                <p className="title-text">TosiDrop</p>
+                <div className="header-filler"></div>
+                <div className="header-wallet-selector noselect">
+                    <WalletSelectorComponent
+                        connectedWallet={connectedWallet}
+                        connectWallet={connectWallet}
+                        wrongNetwork={wrongNetwork}
+                    />
+                </div>
+                <div className="last">
+                    <button
+                        className="button light-button"
+                        onClick={toggleTheme}
+                    >
+                        <FontAwesomeIcon icon={faSun} />
+                    </button>
+                    <button className="button menu-button" onClick={toggleMenu}>
+                        <FontAwesomeIcon icon={faBars} />
+                    </button>
+                </div>
             </div>
         </div>
-    </div>
+    );
 }
 
 export default Header;
