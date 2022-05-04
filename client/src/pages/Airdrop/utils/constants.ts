2a32b1ade3833fd94a575af44a5e4bf825f494e4
aryelciu001 <ryelcius.alfredo29@gmail.com>
2022-05-04 20:24:31 +0800

    chore/client: use new registry endpoint

--- client/src/pages/Airdrop/utils/constants.ts
+++ client/src/pages/Airdrop/utils/constants.ts
@@ -1,5 +1,5 @@
 export const AIRDROP_API_TX = "https://tn-airdrop-api.tosidrop.io";
-export const AIRDROP_API_REG = "https://token-registry-api.apexpool.info";
+export const AIRDROP_API_REG = "https://tn-registry-api.tosidrop.io";
 export const ERROR = {
     NOT_ENOUGH_ADA: "NOT_ENOUGH_ADA",
 };
