71653c8026f6bc952c16b01837b25933133b928b
Chris Gianelloni <cgianelloni@cloudstruct.net>
2022-05-10 14:19:19 -0400

    fix: mount .env to /app/server
    Signed-off-by: Chris Gianelloni <cgianelloni@cloudstruct.net>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,7 @@ __app=$(cd $(dirname ${BASH_SOURCE[0]}); pwd -P)
 # Check for arguments
 # No arguments passed or first argument begins with dash
 if [[ ${#} -eq 0 ]] || [[ ${1#-} != ${1} ]]; then
+	cd ${__app}/server
 	set -- npm run start "${@}"
 fi
 
