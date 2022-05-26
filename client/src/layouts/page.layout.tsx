e47efed4abb52cf37ccb86ce66708c2b5b6d84b4
aryelciu001 <ryelcius.alfredo29@gmail.com>
2022-05-26 14:58:51 +0700

    chore/client: redirect * route to default route

--- client/src/layouts/page.layout.tsx
+++ client/src/layouts/page.layout.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
 import Rewards from "../pages/Rewards";
 import RewardsHistory from "../pages/RewardsHistory";
@@ -6,6 +6,8 @@ import Feedback from "../pages/Feedback";
 import Airdrop from "../pages/Airdrop";
 import WalletApi from "../services/connectors/wallet.connector";
 import DepositInfoPage from "src/pages/Rewards/DepositInfoPage";
+import Loading from "src/pages/Loading";
+import "./page.layout.scss";
 
 interface Params {
     connectedWallet: WalletApi | undefined;
@@ -37,6 +39,7 @@ function Page({ connectedWallet, wrongNetwork }: Params) {
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/feedback" element={<Feedback />} />
             <Route path="/airdrop" element={<Airdrop />} />
+            <Route path="*" element={<Navigate to="/" />} />
         </Routes>
     );
 }
