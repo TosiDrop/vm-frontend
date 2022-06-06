dc5eb8c85e8f87c7268b56fdc2cf0fd9215ed232
aryelciu001 <ryelcius.alfredo29@gmail.com>
2022-06-06 14:27:34 +0700

    chore/client: use tab width for formatting

--- client/src/services/claim.services.ts
+++ client/src/services/claim.services.ts
@@ -4,79 +4,79 @@ import { GetRewards, GetCustomRewards } from "../entities/vm.entities";
 import axios from "axios";
 
 export async function getRewards(
-  address: string
+    address: string
 ): Promise<GetRewards | undefined> {
-  const response = await axios.get(`/getrewards?address=${address}`);
-  if (response && response.data) {
-    return response.data;
-  }
-  return undefined;
+    const response = await axios.get(`/getrewards?address=${address}`);
+    if (response && response.data) {
+        return response.data;
+    }
+    return undefined;
 }
 
 export async function getCustomRewards(
-  staking_address: string,
-  session_id: string,
-  selected: string
+    staking_address: string,
+    session_id: string,
+    selected: string
 ): Promise<GetCustomRewards | undefined> {
-  const response = await axios.get(
-    `/getcustomrewards?staking_address=${staking_address}&session_id=${session_id}&selected=${selected}`
-  );
-  if (response && response.data) {
-    return response.data;
-  }
-  return undefined;
+    const response = await axios.get(
+        `/getcustomrewards?staking_address=${staking_address}&session_id=${session_id}&selected=${selected}`
+    );
+    if (response && response.data) {
+        return response.data;
+    }
+    return undefined;
 }
 
 export async function getTransactionStatus(
-  txHash: string
+    txHash: string
 ): Promise<TransactionStatus[] | undefined> {
-  const response = await axios.get(`/gettransactionstatus?txHash=${txHash}`);
-  if (response && response.data) {
-    return response.data;
-  }
-  return undefined;
+    const response = await axios.get(`/gettransactionstatus?txHash=${txHash}`);
+    if (response && response.data) {
+        return response.data;
+    }
+    return undefined;
 }
 
 export async function getBlock(): Promise<{ block_no: number }> {
-  const response = await axios.get(`/getblock`);
-  if (response && response.data) {
-    return response.data;
-  }
-  return { block_no: 0 };
+    const response = await axios.get(`/getblock`);
+    if (response && response.data) {
+        return response.data;
+    }
+    return { block_no: 0 };
 }
 
 export async function getEpochParams(): Promise<EpochParams[]> {
-  const response = await axios.get(`/getepochparams`);
-  return response.data[0];
+    const response = await axios.get(`/getepochparams`);
+    return response.data[0];
 }
 
 export async function getNetworkId(): Promise<{ network: NetworkId }> {
-  const response = await axios.get(`/features`);
-  if (response && response.data) {
-    if (response.data.network === "testnet") {
-      return { network: NetworkId.testnet };
+    const response = await axios.get(`/features`);
+    if (response && response.data) {
+        if (response.data.network === "testnet") {
+            return { network: NetworkId.testnet };
+        }
+        return { network: NetworkId.mainnet };
     }
-    return { network: NetworkId.mainnet };
-  }
-  return { network: NetworkId.undefined };
+    return { network: NetworkId.undefined };
 }
 
 export async function getTxStatus(request_id: string, session_id: string) {
-  const response = await axios.get(
-    `/txstatus?request_id=${request_id}&session_id=${session_id}`
-  );
-  return response.data;
+    const response = await axios.get(
+        `/txstatus?request_id=${request_id}&session_id=${session_id}`
+    );
+    return response.data;
 }
 
 export async function getSettings() {
-  const response = await axios.get(`/getsettings`);
-  return response.data;
+    const response = await axios.get(`/getsettings`);
+    return response.data;
 }
 
 export async function getStakeKey(addr: string) {
-  if (addr.slice(0, 5) === "stake") {
-    return { staking_address: addr };
-  }
-  const response = await axios.get(`/getstakekey?address=${addr}`);
-  return response.data;
+    if (addr.slice(0, 5) === "stake") {
+        return { staking_address: addr };
+    }
+    const response = await axios.get(`/getstakekey?address=${addr}`);
+    return response.data;
 }
