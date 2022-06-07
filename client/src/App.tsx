ee67de21928f39f3f8244b7905bee5aa4c05a731
aryelciu001 <ryelcius.alfredo29@gmail.com>
2022-06-07 15:40:06 +0700

    chore/client: move showMenu to redux

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
     connectWallet as connectWalletRedux,
