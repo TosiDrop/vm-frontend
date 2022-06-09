b195bf863e50f2a06cfe0c0868edb73d60633034
Chris Gianelloni <cgianelloni@cloudstruct.net>
2022-06-09 10:34:56 -0400

    chore: remove npm use in container
    Signed-off-by: Chris Gianelloni <cgianelloni@cloudstruct.net>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ __app=$(cd $(dirname ${BASH_SOURCE[0]}); pwd -P)
 # No arguments passed or first argument begins with dash
 if [[ ${#} -eq 0 ]] || [[ ${1#-} != ${1} ]]; then
 	cd ${__app}/server
-	set -- npm run start "${@}"
+	set -- node -r ts-node/register index.ts "${@}"
 fi
 
 exec "${@}"
