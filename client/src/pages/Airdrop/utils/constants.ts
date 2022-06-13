17fdbfd0e07f09837b9c8aebf78eb4e07ca9b715
aryelciu001 <ryelcius.alfredo29@gmail.com>
2022-06-13 22:21:51 +0700

    chore/client: add error msg

--- client/src/pages/Airdrop/utils/constants.ts
+++ client/src/pages/Airdrop/utils/constants.ts
@@ -2,4 +2,5 @@ export const AIRDROP_API_TX = "https://tn-airdrop-api.tosidrop.io";
 export const AIRDROP_API_REG = "https://tn-registry-api.tosidrop.io";
 export const ERROR = {
     NOT_ENOUGH_ADA: "NOT_ENOUGH_ADA",
+    NOT_ENOUGH_TOKENS: "NOT_ENOUGH_TOKENS",
 };
