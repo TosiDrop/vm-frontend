3e664eeb185488016f8d3a73506fc8e64b21f7fd
aryelciu001 <ryelcius.alfredo29@gmail.com>
2022-06-15 19:31:04 +0700

    chore/client: prettier

--- client/src/components/BlockchainSelector/index.tsx
+++ client/src/components/BlockchainSelector/index.tsx
@@ -1,37 +1,44 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
-import CardanoLogo from 'src/assets/cardano-logo.png'
-import ErgoLogo from 'src/assets/ergo-logo.png'
-import useComponentVisible from 'src/hooks/useComponentVisible';
-import './index.scss'
+import CardanoLogo from "src/assets/cardano-logo.png";
+import ErgoLogo from "src/assets/ergo-logo.png";
+import useComponentVisible from "src/hooks/useComponentVisible";
+import "./index.scss";
 
-const CLASS = 'blockchain-selector'
+const CLASS = "blockchain-selector";
 
 const BlockchainSelector = () => {
-  const { ref, visible, setVisible } = useComponentVisible(false)
+    const { ref, visible, setVisible } = useComponentVisible(false);
 
-  const handleClick = () => {
-    setVisible(!visible)
-  }
+    const handleClick = () => {
+        setVisible(!visible);
+    };
 
-  const switchBlockchain = (blockchain: string) => {
+    const switchBlockchain = (blockchain: string) => {};
 
-  }
-
-  return (
-    <div className={`${CLASS} ${'cardano'}`} onClick={() => handleClick()} ref={ref}>
-      <img src={CardanoLogo}></img>
-      <FontAwesomeIcon icon={faCaretDown}></FontAwesomeIcon>
-      <div className={`${CLASS}__select ${visible ? 'visible' : 'hidden'}`}>
-        {/* <div className={`${CLASS}__option cardano` onClick={() => switchBlockchain('cardano')}}>
+    return (
+        <div
+            className={`${CLASS} ${"cardano"}`}
+            onClick={() => handleClick()}
+            ref={ref}
+        >
+            <img src={CardanoLogo}></img>
+            <FontAwesomeIcon icon={faCaretDown}></FontAwesomeIcon>
+            <div
+                className={`${CLASS}__select ${visible ? "visible" : "hidden"}`}
+            >
+                {/* <div className={`${CLASS}__option cardano` onClick={() => switchBlockchain('cardano')}}>
           <img src={CardanoLogo}></img>
         </div> */}
-        <div className={`${CLASS}__option ergo`} onClick={() => switchBlockchain('ergo')}>
-          <img src={ErgoLogo}></img>
+                <div
+                    className={`${CLASS}__option ergo`}
+                    onClick={() => switchBlockchain("ergo")}
+                >
+                    <img src={ErgoLogo}></img>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
-}
+    );
+};
 
-export default BlockchainSelector
+export default BlockchainSelector;
