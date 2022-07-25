f45e16b21e2ace17dfc187281594b6165aa0557f
aryelciu001 <ryelcius.alfredo29@gmail.com>
2022-07-25 22:13:40 +0800

    chore/client: start using tailwind css

--- client/src/pages/Rewards/components/ClaimableTokenBox/index.tsx
+++ client/src/pages/Rewards/components/ClaimableTokenBox/index.tsx
@@ -42,7 +42,7 @@ const ClaimableTokenBox = ({
                 {premium ? (
                     <span className="premium-token">
                         <FontAwesomeIcon icon={faStar} />
-                        <div className="tooltip">
+                        <div className="tooltip w-64 p-3.5 rounded-2xl right-5 bottom-0">
                             The star indicates premium token. Premium token
                             requires premium fee to claim.
                         </div>
