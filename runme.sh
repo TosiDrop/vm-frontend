2fb8df7b74214b2353789720db966a2d36e2d2a5
chadle-git <60274638+chadle-git@users.noreply.github.com>
2022-08-23 16:55:13 -0500

    changed rumme.sh

--- runme.sh
+++ runme.sh
@@ -74,7 +74,8 @@ ansible-playbook ${__repo}/ansible/local.yml \
 	-e MANAGE_DOCKER=${MANAGE_DOCKER:-true} \
 	-e vm_frontend_version=${VM_BRANCH} \
 	-e vm_frontend_port=${PORT:-3000} \
-	-e DATADOG_API_KEY=${DATADOG_API_KEY} \
+	-e DATADOG_API_KEY=${DATADOG_API_KEY:-changeme} \
+	-e MANAGE_DATADOG=${MANAGE_DATADOG:-false}
 	-e ansible_python_interpreter=${__repo}/.venv/bin/python3 \
 	--diff \
 	-c local \
