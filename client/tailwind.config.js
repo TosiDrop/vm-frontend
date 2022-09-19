74ee9f293f8f6e8781a80f16eb99c04747fded88
aryelciu001 <ryelcius.alfredo29@gmail.com>
2022-09-19 23:31:31 +0800

    chore/web: smaller footer text for phone

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontSize: {
+        xxs: "0.6rem",
+      },
       minWidth: {
-        '40': '10rem',
-        '52': '13rem',
+        40: "10rem",
+        52: "13rem",
       },
       maxWidth: {
-        '64': '16rem',
-        '52': '13rem',
-      }
+        64: "16rem",
+        52: "13rem",
+      },
     },
   },
   plugins: [],
-}
+};
