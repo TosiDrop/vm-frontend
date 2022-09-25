87bf8d79f911c340392ed55d106948c63aca3d36
aryelciu001 <ryelcius.alfredo29@gmail.com>
2022-09-25 16:26:45 +0800

    chore/web: make max-w bigger, align menu with box

--- client/src/layouts/page.tsx
+++ client/src/layouts/page.tsx
@@ -7,8 +7,11 @@ interface Props {
 
 const Page = ({ children }: Props) => {
   return (
-    <div className="w-full max-w-7xl p-5 pt-14 flex flex-row gap-8">
-      <Menu></Menu>
+    <div className="w-full max-w-8xl p-5 pt-14 flex flex-row gap-8">
+      <div className="flex flex-col gap-8">
+        <p className="text-3xl opacity-0">_</p>
+        <Menu></Menu>
+      </div>
       <MobileMenu></MobileMenu>
       <div className="w-full flex flex-col gap-8">{children}</div>
     </div>
