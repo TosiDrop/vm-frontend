dc226a7ea042d325042a6d5882e3ff0a321417fb
BNeutral <BNeutral@users.noreply.github.com>
2022-09-26 06:32:41 -0300

    Forgot to run formatter

--- client/src/pages/Projects/index.tsx
+++ client/src/pages/Projects/index.tsx
@@ -16,12 +16,11 @@ import {
 function Projects() {
   return (
     <Page>
-      <div className="text-3xl">Explore TosiDrop Projects
+      <div className="text-3xl">
+        Explore TosiDrop Projects
         <ul>
-          {sampleData.map( (entry) => {
-            return <li>
-              {RenderProjectButton(entry)}
-            </li>
+          {sampleData.map((entry) => {
+            return <li>{RenderProjectButton(entry)}</li>;
           })}
         </ul>
       </div>
@@ -31,43 +30,70 @@ function Projects() {
 
 function RenderProjectButton(data: ProjectData): JSX.Element {
   const { theme } = useSelector((state: RootState) => state.global);
-  let logo = (theme === Themes.dark && data.logoDark) ? data.logoDark : data.logoDefault;
+  let logo =
+    theme === Themes.dark && data.logoDark ? data.logoDark : data.logoDefault;
   let compactLogo = data.logoCompact ?? data.logoDefault;
 
   return (
     <div className="rounded-2xl background px-2.5 py-2.5 mt-5 mb-5 items-center text-base text-left flex flex-wrap md:flex-row">
-
       <div className="basis-7/12 flex flex-row grow items-center">
         <div className="m-2.5 p-2.5 basis-3/12">
           <img src={logo} className="logo hidden sm:block"></img>
           <img src={compactLogo} className="logo sm:hidden"></img>
         </div>
-        <div className="m-2.5 p-2.5 basis-9/12 grow md:border-r">{data.descShort}</div>     
+        <div className="m-2.5 p-2.5 basis-9/12 grow md:border-r">
+          {data.descShort}
+        </div>
       </div>
 
       <div className="basis-5/12 flex flex-row grow items-center">
-        <div className="m-2.5 p-2.5 basis-1/4 "><div>Token:</div><div className="font-bold">{data.token}</div></div>
-        <div className="m-2.5 p-2.5 basis-1/4 "><div>Total Supply:</div><div className="font-bold text-sm">{data.totalSupply.toLocaleString('en-US')}</div></div>
-        <div className="m-2.5 p-2.5 basis-2/4 text-2xl">{RenderProjectURLLinks(data.urls)}</div>
+        <div className="m-2.5 p-2.5 basis-1/4 ">
+          <div>Token:</div>
+          <div className="font-bold">{data.token}</div>
+        </div>
+        <div className="m-2.5 p-2.5 basis-1/4 ">
+          <div>Total Supply:</div>
+          <div className="font-bold text-sm">
+            {data.totalSupply.toLocaleString("en-US")}
+          </div>
+        </div>
+        <div className="m-2.5 p-2.5 basis-2/4 text-2xl">
+          {RenderProjectURLLinks(data.urls)}
+        </div>
       </div>
-      
     </div>
-  )
+  );
 }
 
 function RenderProjectURLLinks(urls: ProjectURLs): JSX.Element[] {
   var elements: JSX.Element[] = [];
   if (urls.medium) {
-    elements.push(<a href={urls.medium} className="text-medium m-1"><FontAwesomeIcon icon={faMedium}/></a>);
+    elements.push(
+      <a href={urls.medium} className="text-medium m-1">
+        <FontAwesomeIcon icon={faMedium} />
+      </a>
+    );
   }
   if (urls.twitter) {
-    elements.push(<a href={urls.twitter} className="text-twitter m-1"><FontAwesomeIcon icon={faTwitter}/></a>);
+    elements.push(
+      <a href={urls.twitter} className="text-twitter m-1">
+        <FontAwesomeIcon icon={faTwitter} />
+      </a>
+    );
   }
   if (urls.discord) {
-    elements.push(<a href={urls.discord} className="text-discord m-1"><FontAwesomeIcon icon={faDiscord}/></a>);
+    elements.push(
+      <a href={urls.discord} className="text-discord m-1">
+        <FontAwesomeIcon icon={faDiscord} />
+      </a>
+    );
   }
   if (urls.telegram) {
-    elements.push(<a href={urls.telegram} className="text-telegram m-1"><FontAwesomeIcon icon={faTelegram}/></a>);
+    elements.push(
+      <a href={urls.telegram} className="text-telegram m-1">
+        <FontAwesomeIcon icon={faTelegram} />
+      </a>
+    );
   }
   /*if (urls.cardanoExplorer) {
     elements.push(<a href={urls.cardanoExplorer}><img src=""/></a>);
@@ -77,6 +103,5 @@ function RenderProjectURLLinks(urls: ProjectURLs): JSX.Element[] {
   }*/
   return elements;
 }
-
 
 export default Projects;
--- client/src/pages/Projects/projectData.tsx
+++ client/src/pages/Projects/projectData.tsx
@@ -2,103 +2,110 @@ import LogoTosiLight from "src/assets/tosidrop-light.png";
 import LogoTosiDark from "src/assets/tosidrop-dark.png";
 import LogoTosiCompact from "src/assets/tosidrop_logo.png";
 
-
 export interface ProjectURLs {
-    medium?: string;
-    twitter?: string;
-    discord?: string;
-    telegram?: string;
-    website?: string;
-    litepaper?: string;
-    docs?: string;
-    cardanoExplorer?: string;
-    poolpm?: string;
+  medium?: string;
+  twitter?: string;
+  discord?: string;
+  telegram?: string;
+  website?: string;
+  litepaper?: string;
+  docs?: string;
+  cardanoExplorer?: string;
+  poolpm?: string;
 }
 
 export interface ProjectData {
-    logoDefault: string;
-    logoDark?: string;
-    logoCompact?: string;
+  logoDefault: string;
+  logoDark?: string;
+  logoCompact?: string;
 
-    descShort: string;
-    descLong: string;
-    claimDesc: string;
+  descShort: string;
+  descLong: string;
+  claimDesc: string;
 
-    token: string;
-    totalSupply: number;
-    policyID: string;
-    /*marketCap: number;
+  token: string;
+  totalSupply: number;
+  policyID: string;
+  /*marketCap: number;
     fdMarketCap: number;*/
 
-    urls: ProjectURLs;
+  urls: ProjectURLs;
 }
 
 // TODO: Fill with the actual data
 export const sampleData: ProjectData[] = [
-    { // Aneta
-        logoDefault: LogoTosiLight,
-        logoDark: LogoTosiDark,
-        logoCompact: LogoTosiCompact,
-        descShort: "AnetaBTC is a fully on-chain, decentralized protocol that allows Bitcoin to be directly wrapped on the Ergo and Cardano blockchains.",
-        descLong: "AnetaBTC is a fully on-chain, decentralized protocol that allows Bitcoin to be directly wrapped on the Ergo and Cardano blockchains. Rooted in the fundamentals of decentralization, anetaBTC is backed by a strong global community, and is developing infrastrucrture throughout both Cardano and Ergo to advance the adoption and functionality of both platforms. anetaBTC is on target to release their wrapping protocol on the Ergo tesnet in Q4 2022. cNETA, the Cardano native token of anetaBTC, represents governance and revenue rights over the anetBTC protocol, including ownership of anetaBTC’s community owned NETA Liquidity Fund, which holds 8+ assets and over $1 million in total value.",
-        claimDesc: "Must be staking to one of the follwing pools: NETA1, NETA2, ...",
-        token: "cNETA",
-        totalSupply: 1000000000,
-        policyID: "b34b3ea80060ace9427bda98690a73d33840e27aaa8d6edb7f0c757a",
-        urls: {
-            website: "http://example.com",
-            medium: "http://example.com",
-            twitter: "http://example.com",
-            discord: "http://example.com",
-            telegram: "http://example.com",
-            litepaper: "http://example.com",
-            docs: "http://example.com",
-            cardanoExplorer: "http://example.com",
-            poolpm: "http://example.com",
-        },
+  {
+    // Aneta
+    logoDefault: LogoTosiLight,
+    logoDark: LogoTosiDark,
+    logoCompact: LogoTosiCompact,
+    descShort:
+      "AnetaBTC is a fully on-chain, decentralized protocol that allows Bitcoin to be directly wrapped on the Ergo and Cardano blockchains.",
+    descLong:
+      "AnetaBTC is a fully on-chain, decentralized protocol that allows Bitcoin to be directly wrapped on the Ergo and Cardano blockchains. Rooted in the fundamentals of decentralization, anetaBTC is backed by a strong global community, and is developing infrastrucrture throughout both Cardano and Ergo to advance the adoption and functionality of both platforms. anetaBTC is on target to release their wrapping protocol on the Ergo tesnet in Q4 2022. cNETA, the Cardano native token of anetaBTC, represents governance and revenue rights over the anetBTC protocol, including ownership of anetaBTC’s community owned NETA Liquidity Fund, which holds 8+ assets and over $1 million in total value.",
+    claimDesc:
+      "Must be staking to one of the follwing pools: NETA1, NETA2, ...",
+    token: "cNETA",
+    totalSupply: 1000000000,
+    policyID: "b34b3ea80060ace9427bda98690a73d33840e27aaa8d6edb7f0c757a",
+    urls: {
+      website: "http://example.com",
+      medium: "http://example.com",
+      twitter: "http://example.com",
+      discord: "http://example.com",
+      telegram: "http://example.com",
+      litepaper: "http://example.com",
+      docs: "http://example.com",
+      cardanoExplorer: "http://example.com",
+      poolpm: "http://example.com",
     },
-    { // Tosi
-        logoDefault: LogoTosiLight,
-        logoDark: LogoTosiDark,
-        logoCompact: LogoTosiCompact,
-        descShort: "TosiDrop is a token distribution platform on Cardano and Ergo that allows projects in the ecosystem to seamlessly distribute tokens to their community members.",
-        descLong: "",
-        claimDesc: "",
-        token: "cTOSI",
-        totalSupply: 1000000000,
-        policyID: "b34b3ea80060ace9427bda98690a73d33840e27aaa8d6edb7f0c757a",
-        urls: {
-        website: "http://example.com",
-        medium: "http://example.com",
-        twitter: "http://example.com",
-        discord: "http://example.com",
-        telegram: "http://example.com",
-        litepaper: "http://example.com",
-        docs: "http://example.com",
-        cardanoExplorer: "http://example.com",
-        poolpm: "http://example.com",
-        },
+  },
+  {
+    // Tosi
+    logoDefault: LogoTosiLight,
+    logoDark: LogoTosiDark,
+    logoCompact: LogoTosiCompact,
+    descShort:
+      "TosiDrop is a token distribution platform on Cardano and Ergo that allows projects in the ecosystem to seamlessly distribute tokens to their community members.",
+    descLong: "",
+    claimDesc: "",
+    token: "cTOSI",
+    totalSupply: 1000000000,
+    policyID: "b34b3ea80060ace9427bda98690a73d33840e27aaa8d6edb7f0c757a",
+    urls: {
+      website: "http://example.com",
+      medium: "http://example.com",
+      twitter: "http://example.com",
+      discord: "http://example.com",
+      telegram: "http://example.com",
+      litepaper: "http://example.com",
+      docs: "http://example.com",
+      cardanoExplorer: "http://example.com",
+      poolpm: "http://example.com",
     },
-    { // SCAT DAO
-        logoDefault: LogoTosiLight,
-        logoDark: LogoTosiDark,
-        logoCompact: LogoTosiCompact,
-        descShort: "SCAT DAO is bringing decentralized audits, research, and safety to Cardano. Token holders can vote on which projects they would like to be audited.",
-        descLong: "",
-        claimDesc: "",
-        token: "AUDIT",
-        totalSupply: 1000000000,
-        policyID: "b34b3ea80060ace9427bda98690a73d33840e27aaa8d6edb7f0c757a",
-        urls: {
-        website: "http://example.com",
-        medium: "http://example.com",
-        twitter: "http://example.com",
-        discord: "http://example.com",
-        telegram: "http://example.com",
-        litepaper: "http://example.com",
-        docs: "http://example.com",
-        cardanoExplorer: "http://example.com",
-        poolpm: "http://example.com",
-        },
+  },
+  {
+    // SCAT DAO
+    logoDefault: LogoTosiLight,
+    logoDark: LogoTosiDark,
+    logoCompact: LogoTosiCompact,
+    descShort:
+      "SCAT DAO is bringing decentralized audits, research, and safety to Cardano. Token holders can vote on which projects they would like to be audited.",
+    descLong: "",
+    claimDesc: "",
+    token: "AUDIT",
+    totalSupply: 1000000000,
+    policyID: "b34b3ea80060ace9427bda98690a73d33840e27aaa8d6edb7f0c757a",
+    urls: {
+      website: "http://example.com",
+      medium: "http://example.com",
+      twitter: "http://example.com",
+      discord: "http://example.com",
+      telegram: "http://example.com",
+      litepaper: "http://example.com",
+      docs: "http://example.com",
+      cardanoExplorer: "http://example.com",
+      poolpm: "http://example.com",
     },
-  ]
+  },
+];
