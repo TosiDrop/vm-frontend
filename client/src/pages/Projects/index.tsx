92f7dc0d591e48c96252c1c8a078483ebdb52a04
BNeutral <BNeutral@users.noreply.github.com>
2022-10-11 03:22:19 -0300

    Moving projects data away from classes

--- client/src/pages/Projects/index.tsx
+++ client/src/pages/Projects/index.tsx
@@ -1,11 +1,110 @@
 import Page from "src/layouts/page";
-import { sampleData } from "./projectData";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faDiscord,
+  faTelegram,
+  faMedium,
+} from "@fortawesome/free-brands-svg-icons";
+import { Themes } from "src/entities/common.entities";
+import {
+  sampleData,
+  ProjectData,
+  ProjectLogos,
+  ProjectURLs,
+} from "./projectData";
 
 import { RootState } from "src/store";
 import { useSelector } from "react-redux";
 
-function Projects() {
+const Projects = () => {
   const { theme } = useSelector((state: RootState) => state.global);
+
+  const renderProject = (projectData: ProjectData) => {
+    return (
+      <div className="rounded-2xl background px-2.5 py-2.5 mt-5 mb-5 items-center text-base text-left flex flex-wrap md:flex-row">
+        <div className="basis-7/12 flex flex-row grow items-center">
+          <div className="m-2.5 p-2.5 basis-3/12">
+            {renderProjectLogos(projectData.logos)}
+          </div>
+          <div className="m-2.5 p-2.5 basis-9/12 grow md:border-r">
+            {projectData.descs.descShort}
+          </div>
+        </div>
+
+        <div className="basis-5/12 flex flex-row grow items-center">
+          <div className="m-2.5 p-2.5 basis-1/4 ">
+            <div>Token:</div>
+            <div className="font-bold">{projectData.token.token}</div>
+          </div>
+          <div className="m-2.5 p-2.5 basis-1/4 ">
+            <div>Total Supply:</div>
+            <div className="font-bold text-sm">
+              {projectData.token.totalSupply.toLocaleString("en-US")}
+            </div>
+          </div>
+          <div className="m-2.5 p-2.5 basis-2/4 text-2xl">
+            {renderProjectURLs(projectData.urls)}
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const renderProjectLogos = (projectLogos: ProjectLogos) => {
+    let logo = projectLogos.logoDefault;
+    let compactLogo = projectLogos.logoCompact ?? projectLogos.logoDefault;
+    if (theme === Themes.dark) {
+      if (projectLogos.logoDark) logo = projectLogos.logoDark;
+      if (projectLogos.logoCompactDark)
+        compactLogo = projectLogos.logoCompactDark;
+    }
+    return (
+      <div>
+        <img src={logo} className="logo hidden sm:block"></img>
+        <img src={compactLogo} className="logo sm:hidden"></img>
+      </div>
+    );
+  };
+
+  const renderProjectURLs = (projectURLs: ProjectURLs) => {
+    var elements: JSX.Element[] = [];
+    if (projectURLs.medium) {
+      elements.push(
+        <a href={projectURLs.medium} className="text-medium m-1">
+          <FontAwesomeIcon icon={faMedium} />
+        </a>
+      );
+    }
+    if (projectURLs.twitter) {
+      elements.push(
+        <a href={projectURLs.twitter} className="text-twitter m-1">
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+      );
+    }
+    if (projectURLs.discord) {
+      elements.push(
+        <a href={projectURLs.discord} className="text-discord m-1">
+          <FontAwesomeIcon icon={faDiscord} />
+        </a>
+      );
+    }
+    if (projectURLs.telegram) {
+      elements.push(
+        <a href={projectURLs.telegram} className="text-telegram m-1">
+          <FontAwesomeIcon icon={faTelegram} />
+        </a>
+      );
+    }
+    /*if (projectURLs.cardanoScan) {
+      elements.push(<a href={projectURLs.cardanoScan}><img src=""/></a>);
+    }
+    if (projectURLs.poolpm) {
+      elements.push(<a href={projectURLs.poolpm}><img src=""/></a>);
+    }*/
+    return elements;
+  };
 
   return (
     <Page>
@@ -13,12 +112,12 @@ function Projects() {
         Explore TosiDrop Projects
         <ul>
           {sampleData.map((entry) => {
-            return <li>{entry.render(theme)}</li>;
+            return <li>{renderProject(entry)}</li>;
           })}
         </ul>
       </div>
     </Page>
   );
-}
+};
 
 export default Projects;
--- client/src/pages/Projects/projectData.tsx
+++ client/src/pages/Projects/projectData.tsx
@@ -1,12 +1,3 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faTwitter,
-  faDiscord,
-  faTelegram,
-  faMedium,
-} from "@fortawesome/free-brands-svg-icons";
-import { Themes } from "src/entities/common.entities";
-
 import LogoAneta from "src/assets/projectLogos/aneta-full-light.png";
 import LogoAnetaDark from "src/assets/projectLogos/aneta-full-dark.png";
 import LogoAnetaCompact from "src/assets/projectLogos/aneta-compact-light.png";
@@ -19,223 +10,133 @@ import LogoMinswapCompactDark from "src/assets/projectLogos/minswap-compact-dark
 import LogoArdana from "src/assets/projectLogos/ardana-full-dark.png";
 import LogoArdanaCompact from "src/assets/projectLogos/ardana-compact-dark.png";
 
-export class ProjectURLs {
-  constructor(
-    public website?: string,
-    public medium?: string,
-    public twitter?: string,
-    public discord?: string,
-    public telegram?: string,
-    public whitepaper?: string,
-    public docs?: string,
-    public github?: string,
-    public cardanoScan?: string,
-    public poolpm?: string
-  ) {}
-
-  render(): JSX.Element[] {
-    var elements: JSX.Element[] = [];
-    if (this.medium) {
-      elements.push(
-        <a href={this.medium} className="text-medium m-1">
-          <FontAwesomeIcon icon={faMedium} />
-        </a>
-      );
-    }
-    if (this.twitter) {
-      elements.push(
-        <a href={this.twitter} className="text-twitter m-1">
-          <FontAwesomeIcon icon={faTwitter} />
-        </a>
-      );
-    }
-    if (this.discord) {
-      elements.push(
-        <a href={this.discord} className="text-discord m-1">
-          <FontAwesomeIcon icon={faDiscord} />
-        </a>
-      );
-    }
-    if (this.telegram) {
-      elements.push(
-        <a href={this.telegram} className="text-telegram m-1">
-          <FontAwesomeIcon icon={faTelegram} />
-        </a>
-      );
-    }
-    /*if (urls.cardanoScan) {
-      elements.push(<a href={urls.cardanoScan}><img src=""/></a>);
-    }
-    if (urls.poolpm) {
-      elements.push(<a href={urls.poolpm}><img src=""/></a>);
-    }*/
-    return elements;
-  }
+export interface ProjectURLs {
+  website?: string;
+  medium?: string;
+  twitter?: string;
+  discord?: string;
+  telegram?: string;
+  paper?: string;
+  docs?: string;
+  github?: string;
+  cardanoScan?: string;
+  poolpm?: string;
 }
 
-export class ProjectLogos {
-  constructor(
-    public logoDefault: string,
-    public logoDark?: string,
-    public logoCompact?: string,
-    public logoCompactDark?: string
-  ) {}
-
-  render(theme: Themes): JSX.Element {
-    let logo = this.logoDefault;
-    let compactLogo = this.logoCompact ?? this.logoDefault;
-    if (theme === Themes.dark) {
-      if (this.logoDark) logo = this.logoDark;
-      if (this.logoCompactDark) compactLogo = this.logoCompactDark;
-    }
-    return (
-      <div>
-        <img src={logo} className="logo hidden sm:block"></img>
-        <img src={compactLogo} className="logo sm:hidden"></img>
-      </div>
-    );
-  }
+export interface ProjectLogos {
+  logoDefault: string;
+  logoDark?: string;
+  logoCompact?: string;
+  logoCompactDark?: string;
 }
 
-export class ProjectDescs {
-  constructor(
-    public descShort: string,
-    public descLong?: string,
-    public claimDesc?: string
-  ) {}
+export interface ProjectDescs {
+  descShort: string;
+  descLong?: string;
+  claimDesc?: string;
 }
 
-export class ProjectTokenInfo {
-  constructor(
-    public token: string,
-    public totalSupply: number,
-    public policyID: string
-  ) //public marketCap: number; //Need to pull this info dynamically, will likely be a method instead of an member
-  //public fdMarketCap: number; //Need to pull this info dynamically, will likely be a method instead of an member
-  {}
+export interface ProjectTokenInfo {
+  token: string;
+  totalSupply: number;
+  policyID: string;
+  //marketCap: number; //Need to pull this info dynamically, will likely be a method instead of an member
+  //fdMarketCap: number; //Need to pull this info dynamically, will likely be a method instead of an member
 }
 
-export class ProjectData {
-  constructor(
-    public logos: ProjectLogos,
-    public descs: ProjectDescs,
-    public token: ProjectTokenInfo,
-    public urls: ProjectURLs
-  ) {}
-
-  render(theme: Themes): JSX.Element {
-    return (
-      <div className="rounded-2xl background px-2.5 py-2.5 mt-5 mb-5 items-center text-base text-left flex flex-wrap md:flex-row">
-        <div className="basis-7/12 flex flex-row grow items-center">
-          <div className="m-2.5 p-2.5 basis-3/12">
-            {this.logos.render(theme)}
-          </div>
-          <div className="m-2.5 p-2.5 basis-9/12 grow md:border-r">
-            {this.descs.descShort}
-          </div>
-        </div>
-
-        <div className="basis-5/12 flex flex-row grow items-center">
-          <div className="m-2.5 p-2.5 basis-1/4 ">
-            <div>Token:</div>
-            <div className="font-bold">{this.token.token}</div>
-          </div>
-          <div className="m-2.5 p-2.5 basis-1/4 ">
-            <div>Total Supply:</div>
-            <div className="font-bold text-sm">
-              {this.token.totalSupply.toLocaleString("en-US")}
-            </div>
-          </div>
-          <div className="m-2.5 p-2.5 basis-2/4 text-2xl">
-            {this.urls.render()}
-          </div>
-        </div>
-      </div>
-    );
-  }
+export interface ProjectData {
+  logos: ProjectLogos;
+  descs: ProjectDescs;
+  token: ProjectTokenInfo;
+  urls: ProjectURLs;
 }
 
 export const sampleData: ProjectData[] = [
-  new ProjectData(
-    new ProjectLogos(LogoAneta, LogoAnetaDark, LogoAnetaCompact, undefined),
-    new ProjectDescs(
-      "AnetaBTC is a fully on-chain, decentralized protocol that allows Bitcoin to be directly wrapped on the Ergo and Cardano blockchains.",
-      "AnetaBTC is a fully on-chain, decentralized protocol that allows Bitcoin to be directly wrapped on the Ergo and Cardano blockchains. Rooted in the fundamentals of decentralization, anetaBTC is backed by a strong global community, and is developing infrastrucrture throughout both Cardano and Ergo to advance the adoption and functionality of both platforms. anetaBTC is on target to release their wrapping protocol on the Ergo tesnet in Q4 2022. cNETA, the Cardano native token of anetaBTC, represents governance and revenue rights over the anetBTC protocol, including ownership of anetaBTC’s community owned NETA Liquidity Fund, which holds 8+ assets and over $1 million in total value.",
-      "Must be staking to one of the follwing pools: NETA1, NETA2, ..."
-    ),
-    new ProjectTokenInfo(
-      "cNETA",
-      1000000000,
-      "b34b3ea80060ace9427bda98690a73d33840e27aaa8d6edb7f0c757a"
-    ),
-    new ProjectURLs(
-      "https://anetabtc.io/", // website
-      "https://medium.com/@anetaBTC", // medium
-      "https://twitter.com/anetaBTC", // twitter
-      "https://discord.com/invite/ScXG76dJXM", // discord
-      "https://t.me/anetaBTC", // telegram
-      "https://medium.com/@anetaBTC/anetabtc-litepaper-v1-0-171f29b3276a", // paper
-      undefined, // docs
-      undefined, // github
-      "https://cardanoscan.io/token/b34b3ea80060ace9427bda98690a73d33840e27aaa8d6edb7f0c757a634e455441", // cardanoeScan
-      undefined // poolpm
-    )
-  ),
+  {
+    // Aneta
+    logos: {
+      logoDefault: LogoAneta,
+      logoDark: LogoAnetaDark,
+      logoCompact: LogoAnetaCompact,
+    },
+    descs: {
+      descShort:
+        "AnetaBTC is a fully on-chain, decentralized protocol that allows Bitcoin to be directly wrapped on the Ergo and Cardano blockchains.",
+      descLong:
+        "AnetaBTC is a fully on-chain, decentralized protocol that allows Bitcoin to be directly wrapped on the Ergo and Cardano blockchains. Rooted in the fundamentals of decentralization, anetaBTC is backed by a strong global community, and is developing infrastrucrture throughout both Cardano and Ergo to advance the adoption and functionality of both platforms. anetaBTC is on target to release their wrapping protocol on the Ergo tesnet in Q4 2022. cNETA, the Cardano native token of anetaBTC, represents governance and revenue rights over the anetBTC protocol, including ownership of anetaBTC’s community owned NETA Liquidity Fund, which holds 8+ assets and over $1 million in total value.",
+      claimDesc:
+        "Must be staking to one of the follwing pools: NETA1, NETA2, ...",
+    },
+    token: {
+      token: "cNETA",
+      totalSupply: 1000000000,
+      policyID: "b34b3ea80060ace9427bda98690a73d33840e27aaa8d6edb7f0c757a",
+    },
+    urls: {
+      website: "https://anetabtc.io/",
+      medium: "https://medium.com/@anetaBTC",
+      twitter: "https://twitter.com/anetaBTC",
+      discord: "https://discord.com/invite/ScXG76dJXM",
+      telegram: "https://t.me/anetaBTC",
+      paper:
+        "https://medium.com/@anetaBTC/anetabtc-litepaper-v1-0-171f29b3276a",
+      cardanoScan:
+        "https://cardanoscan.io/token/b34b3ea80060ace9427bda98690a73d33840e27aaa8d6edb7f0c757a634e455441",
+    },
+  },
 
-  new ProjectData(
-    new ProjectLogos(
-      LogoMinswap,
-      LogoMinswapDark,
-      LogoMinswapCompact,
-      LogoMinswapCompactDark
-    ),
-    new ProjectDescs(
-      "Minswap is a Decentralized Exchange (DEX)",
-      undefined,
-      undefined
-    ),
-    new ProjectTokenInfo(
-      "MIN",
-      5000000000,
-      "29d222ce763455e3d7a09a665ce554f00ac89d2e99a1a83d267170c6"
-    ),
-    new ProjectURLs(
-      "https://minswap.org/", // website
-      "https://medium.com/minswap", // medium
-      "https://twitter.com/minswapdex", // twitter
-      "https://discord.gg/ZjB8ZBhkbm", // discord
-      "https://t.me/MinswapMafia", // telegram
-      "https://docs.minswap.org/whitepaper", // paper
-      "https://docs.minswap.org/", // docs
-      undefined, // github
-      "https://cardanoscan.io/token/29d222ce763455e3d7a09a665ce554f00ac89d2e99a1a83d267170c64d494e", // cardanoScan
-      undefined // poolpm
-    )
-  ),
+  {
+    // Minswap
+    logos: {
+      logoDefault: LogoMinswap,
+      logoDark: LogoMinswapDark,
+      logoCompact: LogoMinswapCompact,
+      logoCompactDark: LogoMinswapCompactDark,
+    },
+    descs: {
+      descShort: "Minswap is a Decentralized Exchange (DEX)",
+    },
+    token: {
+      token: "MIN",
+      totalSupply: 5000000000,
+      policyID: "29d222ce763455e3d7a09a665ce554f00ac89d2e99a1a83d267170c6",
+    },
+    urls: {
+      website: "https://minswap.org/",
+      medium: "https://medium.com/minswap",
+      twitter: "https://twitter.com/minswapdex",
+      discord: "https://discord.gg/ZjB8ZBhkbm",
+      telegram: "https://t.me/MinswapMafia",
+      paper: "https://docs.minswap.org/whitepaper",
+      docs: "https://docs.minswap.org/",
+      cardanoScan:
+        "https://cardanoscan.io/token/29d222ce763455e3d7a09a665ce554f00ac89d2e99a1a83d267170c64d494e",
+    },
+  },
 
-  new ProjectData(
-    new ProjectLogos(LogoArdana, undefined, LogoArdanaCompact, undefined),
-    new ProjectDescs(
-      "Ardana is a decentralized stablecoin hub which will bring the necessary DeFi primitives needed to bootstrap & maintain any economy to Cardano.",
-      undefined,
-      undefined
-    ),
-    new ProjectTokenInfo(
-      "DANA",
-      125000000,
-      "c88bbd1848db5ea665b1fffbefba86e8dcd723b5085348e8a8d2260f"
-    ),
-    new ProjectURLs(
-      "https://ardana.org/", // website
-      "https://medium.com/ardana-hub", // medium
-      "https://twitter.com/ardanaproject", // twitter
-      "https://discord.gg/deJvmdTamu", // discord
-      "https://t.me/ardanaofficial", // telegram
-      "", // paper
-      "https://docs.ardana.org/", // docs
-      undefined, // github
-      "https://cardanoscan.io/token/c88bbd1848db5ea665b1fffbefba86e8dcd723b5085348e8a8d2260f44414e41", // cardanoScan
-      undefined // poolpm
-    )
-  ),
+  {
+    logos: {
+      logoDefault: LogoArdana,
+      logoCompact: LogoArdanaCompact,
+    },
+    descs: {
+      descShort:
+        "Ardana is a decentralized stablecoin hub which will bring the necessary DeFi primitives needed to bootstrap & maintain any economy to Cardano.",
+    },
+    token: {
+      token: "DANA",
+      totalSupply: 125000000,
+      policyID: "c88bbd1848db5ea665b1fffbefba86e8dcd723b5085348e8a8d2260f",
+    },
+    urls: {
+      website: "https://ardana.org/",
+      medium: "https://medium.com/ardana-hub",
+      twitter: "https://twitter.com/ardanaproject",
+      discord: "https://discord.gg/deJvmdTamu",
+      telegram: "https://t.me/ardanaofficial",
+      docs: "https://docs.ardana.org/",
+      cardanoScan:
+        "https://cardanoscan.io/token/c88bbd1848db5ea665b1fffbefba86e8dcd723b5085348e8a8d2260f44414e41",
+    },
+  },
 ];
