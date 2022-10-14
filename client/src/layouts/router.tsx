f18b57dbdd75fc521fcc221f3e911c069100dde3
aryelciu001 <ryelcius.alfredo29@gmail.com>
2022-10-15 00:13:57 +0800

    chore/client: move project page to cardano

--- client/src/layouts/router.tsx
+++ client/src/layouts/router.tsx
@@ -3,8 +3,8 @@ import { PageRoute } from "src/entities/common.entities";
 import Airdrop from "src/pages/Cardano/Airdrop";
 import Claim from "src/pages/Cardano/Claim";
 import DepositInfoPage from "src/pages/Cardano/Deposit";
+import Projects from "src/pages/Cardano/Projects";
 import Feedback from "src/pages/Feedback";
-import Projects from "src/pages/Projects";
 
 const Router = () => {
   return (
