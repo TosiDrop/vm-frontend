b689f74e54268ddb07bb476d109d24bd937f6caa
Chris Gianelloni <cgianelloni@cloudstruct.net>
2022-12-26 12:56:28 -0500

    feat: parse pool.description as markdown
    Signed-off-by: Chris Gianelloni <cgianelloni@cloudstruct.net>

--- client/src/components/Pool/index.tsx
+++ client/src/components/Pool/index.tsx
@@ -1,6 +1,9 @@
+import { Marked } from '@ts-stack/markdown';
 import { StakePoolInfo } from "src/entities/common.entities";
 
 export default function Pool({ pool }: { pool: StakePoolInfo }) {
+  Marked.setOptions({isNoP: true});
+  let parsed = Marked.parse(`${pool.description}`)
   return (
     <div className="background p-5 rounded-2xl flex flex-row gap-4">
       <div className="h-full w-14 flex items-center justify-center">
@@ -10,7 +13,7 @@ export default function Pool({ pool }: { pool: StakePoolInfo }) {
         <div className="font-extrabold text-lg">
           ({pool.ticker}) {pool.name}
         </div>
-        <div className="break-all">{pool.description}</div>
+        <div className="break-all" dangerouslySetInnerHTML={{ __html: parsed === "null" ? "" : parsed }} />
       </div>
     </div>
   );
