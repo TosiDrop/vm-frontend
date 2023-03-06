eab50bf3059990d69dce0e0c6a1acaaa8e3329f4
aryelciu001 <ryelcius.alfredo29@gmail.com>
2023-03-06 21:49:27 +0800

    dto

--- client/src/entities/dto.ts
+++ client/src/entities/dto.ts
@@ -1,113 +1,198 @@
-import { DeliveredReward } from "./common.entities";
+import { DeliveredReward, TransactionStatusDetail } from "./common.entities";
 import { EpochParams, Tip } from "./koios.entities";
-import { Assets, ClaimableToken, PoolInfo, VmPoolInfo } from "./vm.entities";
+import { PricePair } from "./min.entities";
+import { ProjectData } from "./project.entities";
+import {
+  ClaimableToken,
+  PoolInfo,
+  VmPoolInfo,
+  VmTokenInfoMap,
+} from "./vm.entities";
 
-export interface GetRewardsDto {
-  claimable_tokens: ClaimableToken[];
-  pool_info: VmPoolInfo;
-  total_rewards?: number;
-  consolidated_promises?: Assets;
-  consolidated_rewards?: Assets;
-  nfts?: any[];
-  assets?: Assets;
-  min_balance?: number;
-  vending_address?: string;
-  withdrawal_fee?: string;
-  withdraw_all_tokens_deposit?: number;
-  project_locked_rewards?: {
-    consolidated_promises: Assets;
-    consolidated_rewards: Assets;
-    nfts: any[];
-    assets: Assets;
-  };
+export namespace Dto {
+  export interface BaseDto {
+    response: object;
+    body: object;
+    query: object;
+  }
+
+  export interface GetTxStatus extends BaseDto {
+    query: {
+      request_id: string;
+      session_id: string;
+    };
+    response: {
+      status: TransactionStatusDetail;
+      expected_deposit: number;
+      deposit: number;
+    };
+  }
+
+  export interface GetQueue extends BaseDto {
+    response: {
+      pending_tx: number;
+    };
+  }
+
+  export interface GetPopUpInfo extends BaseDto {
+    response: {
+      title: string;
+      text: string;
+      buttonText: string;
+      buttonLink: string;
+    };
+  }
+
+  export interface GetProjects extends BaseDto {
+    response: ProjectData[];
+  }
+
+  export interface GetEpochParams extends BaseDto {
+    response: EpochParams;
+  }
+
+  export interface GetTip extends BaseDto {
+    response: Tip;
+  }
+
+  export interface GetDeliveredRewards extends BaseDto {
+    query: {
+      staking_address: string;
+    };
+    response: {
+      deliveredRewards: DeliveredReward[];
+    };
+  }
+
+  export interface GetCustomRewards extends BaseDto {
+    query: {
+      staking_address: string;
+      session_id: string;
+      selected: string;
+      unlock: string;
+    };
+    response: {
+      request_id: string;
+      deposit: number;
+      overhead_fee: number;
+      withdrawal_address: string;
+      is_whitelisted: boolean;
+    };
+  }
+
+  export interface GetPrices extends BaseDto {
+    response: Record<string, PricePair>;
+  }
+
+  export interface ServerError extends BaseDto {
+    response: {
+      error: string;
+    };
+  }
+
+  export interface GetPools extends BaseDto {
+    response: {
+      whitelistedPools: PoolInfo[];
+      regularPools: PoolInfo[];
+    };
+  }
+
+  export interface GetTokens extends BaseDto {
+    response: VmTokenInfoMap;
+  }
+
+  export interface GetSettings extends BaseDto {
+    response: {
+      withdrawal_fee: number;
+      epoch: number;
+      switching_epoch: boolean;
+      frontend_version: string;
+      backend_version: string;
+      min_balance: number;
+      confirmations_required: number;
+    };
+  }
+
+  export interface GetSystemInfo extends BaseDto {
+    response: {
+      backend_up: boolean;
+      ntds_up: boolean;
+      pending_tx: number;
+      pending_rewards: number;
+      pending_promises: number;
+      tracked_stake: number;
+      tracked_delegators: number;
+      delivered_rewards: number;
+      pending_withdrawals: number;
+      processed_withdrawals: number;
+      failed_withdrawals: number;
+      uptime: string;
+      uptime_ntds: string;
+      xwallet_size: number;
+      epoch: number;
+    };
+  }
+
+  export interface GetHealth extends BaseDto {
+    response: {
+      status: string;
+    };
+  }
+
+  export interface GetHealthz extends BaseDto {
+    response: {
+      status: string;
+    };
+  }
+
+  export interface GetFeatures extends BaseDto {
+    response: {
+      tosi_fee: number;
+      tosi_fee_whitelist: string;
+      claim_enabled: boolean;
+      network: string;
+      ergo_enabled: boolean;
+    };
+  }
+
+  export interface GetStakeKey extends BaseDto {
+    query: {
+      address: string;
+    };
+    response: {
+      staking_address: string;
+    };
+  }
+
+  export interface GetRewards extends BaseDto {
+    query: {
+      address: string;
+    };
+    response: {
+      claimable_tokens: ClaimableToken[] | undefined;
+      pool_info: VmPoolInfo;
+    };
+  }
+
+  export interface CreateStakeTx extends BaseDto {
+    query: {
+      poolId: string;
+      address: string;
+    };
+    response: {
+      witness: string;
+      txBody: string;
+    };
+  }
+
+  export interface SubmitTx extends BaseDto {
+    body: {
+      signedWitness: string;
+      txBody: string;
+    };
+
+    response: {
+      tx: string;
+    };
+  }
 }
-
-export interface GetQueueDto {
-  pending_tx: number;
-}
-
-export interface GetPoolsDto {
-  whitelistedPools: PoolInfo[];
-  regularPools: PoolInfo[];
-}
-
-export interface GetDeliveredRewardsDto {
-  deliveredRewards: DeliveredReward[];
-}
-
-export interface ServerErrorDto {
-  error: string;
-}
-
-export namespace StakeTxDto {
-  export interface GetTxRequest {
-    poolId: string;
-    address: string;
-  }
-
-  export interface GetTxResponse {
-    witness: string;
-    txBody: string;
-  }
-
-  export interface PostSignedTxRequest {
-    signedWitness: string;
-    txBody: string;
-  }
-
-  export interface PostSignedTxResponse {
-    tx: string;
-  }
-}
-
-export interface GetPopupInfoDto {
-  title: string;
-  text: string;
-  buttonText: string;
-  buttonLink: string;
-}
-
-export namespace ProjectsDto {
-  export interface Url {
-    website?: string;
-    medium?: string;
-    twitter?: string;
-    discord?: string;
-    telegram?: string;
-    paper?: string;
-    docs?: string;
-    github?: string;
-    cardanoScan?: string;
-    poolpm?: string;
-  }
-
-  export interface Logo {
-    logoDefault: string;
-    logoDark?: string;
-    logoCompact?: string;
-    logoCompactDark?: string;
-  }
-
-  export interface Desc {
-    descShort: string;
-    descLong?: string;
-    claimDesc?: string;
-  }
-
-  export interface TokenInfo {
-    token: string;
-    totalSupply?: number;
-    policyID: string;
-  }
-
-  export interface GetProjects {
-    logos: Logo;
-    descs: Desc;
-    token: TokenInfo;
-    urls: Url;
-  }
-}
-
-export type GetEpochParamsDto = EpochParams;
-
-export type GetTipDto = Tip;
