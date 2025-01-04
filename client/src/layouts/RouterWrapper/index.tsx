b0eb192b7e65f7f86cbf41f4ec1248843947190c
Chris Gianelloni <wolf31o2@blinklabs.io>
2025-01-04 11:40:39 -0500

    refactor: disable projects, pools, dashboard, and feedback pages (#547)
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- client/src/layouts/RouterWrapper/index.tsx
+++ client/src/layouts/RouterWrapper/index.tsx
@@ -4,11 +4,11 @@ import { PageRoute } from "src/entities/common.entities";
 import Claim from "src/pages/Cardano/Claim";
 import ClaimHistory from "src/pages/Cardano/ClaimHistory";
 import DepositInfoPage from "src/pages/Cardano/Deposit";
-import Pools from "src/pages/Cardano/Pools";
-import Projects from "src/pages/Cardano/Projects";
+// import Pools from "src/pages/Cardano/Pools";
+// import Projects from "src/pages/Cardano/Projects";
 import ComingSoonPage from "src/pages/ComingSoon";
-import Dashboard from "src/pages/Dashboard";
-import Feedback from "src/pages/Feedback";
+// import Dashboard from "src/pages/Dashboard";
+// import Feedback from "src/pages/Feedback";
 import { RootState } from "src/store";
 
 export default function RouterWrapper() {
@@ -20,10 +20,10 @@ export default function RouterWrapper() {
       <Route path={PageRoute.claimCardano} element={<Claim />} />
       <Route path={PageRoute.historyCardano} element={<ClaimHistory />} />
       <Route path={PageRoute.depositCardano} element={<DepositInfoPage />} />
-      <Route path={PageRoute.projectsCardano} element={<Projects />} />
+      {/* Disabled routes <Route path={PageRoute.projectsCardano} element={<Projects />} />
       <Route path={PageRoute.dashboardCardano} element={<Dashboard />} />
       <Route path={PageRoute.poolsCardano} element={<Pools />} />
-      <Route path={PageRoute.feedback} element={<Feedback />} />
+      <Route path={PageRoute.feedback} element={<Feedback />} /> */}
 
       {ergoEnabled ? (
         <Route path={PageRoute.claimErgo} element={<ComingSoonPage />} />
